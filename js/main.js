aec5cefacefc980925304e0a21e1d9c99e0aacd0
Jose David Gonzalez <programacionjose2@gmail.com>
2024-09-03 18:47:17 -0500

    The project has been updated and completed

--- js/main.js
+++ js/main.js
@@ -46,12 +46,67 @@ bgHeader();
 // swiper services  
 const swiperServices = new Swiper('.services__swiper', {
   loop: true,
-  grabCursor:true,
-  spaceBetween:24,
-  slidesPerview: 'auto',
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
   
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+// Show Scroll Up
+
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scrollup')
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+scrollUp();
+
+
+// Scroll Sections Active Link
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+          sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Scroll Reveal Animation 
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 2500,
+  delay: 400,
+//  reset: true, // Animation repeat
+})
+
+sr.reveal(`.home__content , .services__data, .services__swiper , .footer__container`);
+sr.reveal(`.home__images`, {origin: 'bottom', delay: 1000});
+sr.reveal(`.about__images , .contact__img`, {origin: 'left'});
+sr.reveal(`.about__data , .contact__data`, {origin: 'right'});
+sr.reveal(`.projects__card`, {interval: 100});
+
+
+
